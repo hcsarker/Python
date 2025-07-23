170a1b6be17bf9dc31acfc484be0567d03fc5b63
hcsarker <hcsarker2002@gmail.com>
2025-07-24 03:29:33 +0600

    revieew

---
+++ sum.py
@@ -0,0 +1,4 @@
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+sum = float(first_number) + float(second_number)
+print("The sum is: " + str(sum))
